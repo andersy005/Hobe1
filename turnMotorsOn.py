b5c06b4cd33e9292f1f5199b781d2a4a00fab696
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-07-27 12:16:54 +0200

    moves the motor in both direction and changes the pins

--- turnMotorsOn.py
+++ turnMotorsOn.py
@@ -5,19 +5,26 @@ from time import sleep
 GPIO.setmode(GPIO.BOARD)  # tells RPI.GPIO to use the board numbering on RPI
 
 Motor1A = 16  # pin 16 to turn the motor in one direction 
-Motor1B = 18  # pin 18 to turn the motor in the other direction
-Motor1E = 22  # Enable pin to turn the motor on
+Motor1B = 20  # pin 20 to turn the motor in the other direction
+Motor1E = 21  # Enable pin to turn the motor on
 
 GPIO.setup(Motor1A, GPIO.OUT)
 GPIO.setup(Motor1B, GPIO.OUT)
 GPIO.setup(Motor1E, GPIO.OUT)
 
-print "Turning motor on"
+print "Going forwards"
 GPIO.output(Motor1A, GPIO.HIGH)
 GPIO.output(Motor1B, GPIO.LOW)
 GPIO.output(Motor1E, GPIO.HIGH)
 
-sleep(10)  # turn the motors for 10 seconds then turn them off again.
+sleep(2)
+
+print "Going backwards"
+GPIO.output(Motor1A, GPIO.LOW)
+GPIO.output(Motor1B, GPIO.HIGH)
+GPIO.output(Motor1E, GPIO.HIGH)
+
+sleep(5)  # turn the motors for 10 seconds then turn them off again.
 
 print "Stopping motor"
 GPIO.output(Motor1E, GPIO.LOW)
